d12f53ffe187f89c4ed407e448e7f68541199b52
Julie Kohler <julie.kohler@incontact.com>
2017-06-26 08:30:41 -0600

    Starting point

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Your Name</h1>
+          <p className="App-intro">
+            "A person's name is to that person, the sweetest, most important sound in any language." -Dale Carnegie
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
